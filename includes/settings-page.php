950ae5752432e417e6c913c1f95287c96eec4f92
Thomas Barregren <thomas@kntnt.se>
2018-08-31 18:37:30 +0200

    Updated to latest version.

--- includes/settings-page.php
+++ includes/settings-page.php
@@ -10,11 +10,31 @@
                     <td>
 						<?php switch ( $field['type'] ):
 							case 'text': ?>
-                                <input id="<?php echo $id ?>" type="text" name="<?php echo "{$ns}[$id]" ?>" value="<?php echo $values[ $id ]; ?>">
+                                <input id="<?php echo $id ?>" type="text" name="<?php echo "{$ns}[$id]" ?>" value="<?php echo $values[ $id ]; ?>" size="<?php echo isset( $field['size'] ) ? $field['size'] : 20; ?>">
+                                <br>
+								<?php break; ?>
+							<?php case 'text group': ?>
+                                <fieldset id="<?php echo $id ?>">
+									<?php foreach ( $field['options'] as $slug => $item ): ?>
+                                        <input type="text" name="<?php echo "{$ns}[$id][$slug]" ?>" value="<?php echo $values[ $id ][ $slug ]; ?>" size="<?php echo isset( $field['size'] ) ? $field['size'] : 20; ?>"> <?php echo $item; ?><br>
+                                        <br>
+									<?php endforeach; ?>
+                                </fieldset>
+								<?php break; ?>
+							<?php case 'number': ?>
+                                <input id="<?php echo $id ?>" type="number" name="<?php echo "{$ns}[$id]" ?>" value="<?php echo $values[ $id ]; ?>" <?php if ( isset( $field['min'] ) ): echo 'min="' . $field['min'] . '"'; endif; ?> <?php if ( isset( $field['max'] ) ): echo 'max="' . $field['max'] . '"'; endif; ?> <?php if ( isset( $field['step'] ) ): echo 'step="' . $field['step'] . '"'; endif; ?>>
+                                <br>
+								<?php break; ?>
+							<?php case 'url': ?>
+                                <input id="<?php echo $id ?>" type="url" name="<?php echo "{$ns}[$id]" ?>" value="<?php echo $values[ $id ]; ?>" pattern="<?php echo isset( $field['pattern'] ) ? $field['pattern'] : 'https?//.*'; ?>" size="<?php echo isset( $field['size'] ) ? $field['size'] : 20; ?>>
+                                <br>
+								<?php break; ?>
+							<?php case 'email': ?>
+                                <input id="<?php echo $id ?>" type="email" name="<?php echo "{$ns}[$id]" ?>" value="<?php echo $values[ $id ]; ?>" pattern="<?php echo isset( $field['pattern'] ) ? $field['pattern'] : '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$'; ?>" size="<?php echo isset( $field['size'] ) ? $field['size'] : 20; ?>>
                                 <br>
 								<?php break; ?>
 							<?php case 'text area': ?>
-                                ​<textarea id="<?php echo $id ?>" name="<?php echo "{$ns}[$id]" ?>" rows="<?php echo $field['rows'] ?>" cols="<?php echo $field['cols'] ?>"><?php echo $values[ $id ]; ?></textarea>
+                                <textarea id="<?php echo $id ?>" name="<?php echo "{$ns}[$id]" ?>" rows="<?php echo isset( $field['rows'] ) ? $field['rows'] : 2; ?>" cols="<?php echo isset( $field['cols'] ) ? $field['cols'] : 20; ?>"><?php echo $values[ $id ]; ?></textarea>
                                 <br>
 								<?php break; ?>
 							<?php case 'checkbox': ?>
@@ -23,7 +43,7 @@
 							<?php case 'checkbox group': ?>
                                 <fieldset id="<?php echo $id ?>">
 									<?php foreach ( $field['options'] as $value => $item ): ?>
-                                        <input type="checkbox" name="<?php echo "{$ns}[$id][]" ?>" value="<?php echo $value; ?>" <?php echo in_array( $value, $values[ $id ] ) ? 'checked' : '' ?>><?php echo $item; ?><br>
+                                        <input type="checkbox" name="<?php echo "{$ns}[$id][]" ?>" value="<?php echo $value; ?>" <?php echo in_array( $value, $values[ $id ] ) ? 'checked' : '' ?>> <?php echo $item; ?><br>
                                         <br>
 									<?php endforeach; ?>
                                 </fieldset>
